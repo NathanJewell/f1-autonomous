4f668bd5ca92ddf670b2aa2c16ff2babf37719f0
Nathan Jewell <ncjewell@gmail.com>
2019-04-08 14:06:59 -0700

    ros dependency packages

---
+++ ros/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nathan/f1-autonomous/ros/src
+# Build directory: /home/nathan/f1-autonomous/ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nathan/f1-autonomous/ros/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/nathan/f1-autonomous/ros/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nathan/f1-autonomous/ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/nathan/f1-autonomous/ros/src:$ROS_PACKAGE_PATH"
